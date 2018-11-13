532df1cd9d2775a063df2225b00940f38cbc6b75
kk6 <hiro.ashiya@gmail.com>
2018-11-13 18:35:22 +0900

    Build CLI with fire🔥

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # aeroplast
 Transparent PNG conversion (Mainly for Twitter)
+
+## Usage:
+
+### Install packages
+
+```python
+$ pipenv install
+```
+
+### Convert image
+
+```python
+$ pipenv run python aeroplast.py convert original.png converted.png
+```
+### help
+
+```python
+$ pipenv run python aeroplast.py --help
+```
+
+## Origin of the name
+```
+aeroplast == Bubble wrap == 気泡緩衝材 == プチプチ
+```
+
+## License
+
+MIT
--- aeroplast.py
+++ aeroplast.py
@@ -5,19 +5,34 @@ Transparent PNG conversion
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+import fire
 from PIL import Image
 
 
-def main(src, dest):
-    img = Image.open(src)
-    new_size = tuple(x + 2 for x in img.size)
+def add_transparent_frame(src):
+    original_image = Image.open(src)
+    new_size = tuple(x + 2 for x in original_image.size)
     color = (255, 255, 255, 0)
-    output = Image.new(img.mode, new_size, color)
-    output.paste(img, (1, 1))
-    output.save(dest, quality=100)
+    image = Image.new(original_image.mode, new_size, color)
+    image.paste(original_image, (1, 1))
+    return image
+
+
+class CLI(object):
+    """Transparent PNG conversion"""
+
+    def convert(self, src, dest):
+        """
+        Output image with transparent PNG
+
+        :param src: Input src image filename.
+        :param dest: Output destination filename.
+        :return: None
+
+        """
+        image = add_transparent_frame(src)
+        image.save(dest, quality=100)
 
 
 if __name__ == "__main__":
-    import sys
-    src, dest = sys.argv[1:]
-    main(src, dest)
+    fire.Fire(CLI)
