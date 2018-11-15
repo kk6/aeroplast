ef10215f4906c3fb55536c214df63b2a72493967
kk6 <hiro.ashiya@gmail.com>
2018-11-16 03:36:06 +0900

    Add overwite option.

--- aeroplast/cli.py
+++ aeroplast/cli.py
@@ -9,20 +9,27 @@ from .images import add_transparent_frame
 class CLI(object):
     """Transparent PNG conversion"""
 
-    def convert(self, src, dest=None, prefix="t"):
+    def convert(self, src, dest=None, prefix="t", overwrite=False):
         """
         Output image with transparent PNG
 
         :param src: Input src image filename.
         :param dest: Output destination filename.
+        :param prefix: Prefix added to output image file name.
+        :param overwrite: Whether to overwrite the image or not.
         :return: None
 
         """
         path = Path(src)
         absolute_path = path.expanduser().resolve()
 
+        if overwrite:
+            name = path.name
+        else:
+            name = "-".join([prefix, path.name])
+
         if dest is None:
-            dest = absolute_path.parent / "-".join([prefix, path.name])
+            dest = absolute_path.parent / name
 
         logger.info(f"Open the image: {absolute_path}")
         try:
