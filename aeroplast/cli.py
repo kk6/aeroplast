729a0fcaf0cf1a6db7623f05ae25135e546b0bda
kk6 <hiro.ashiya@gmail.com>
2018-11-16 01:15:56 +0900

    Automatic determination of save destination.

--- aeroplast/cli.py
+++ aeroplast/cli.py
@@ -9,7 +9,7 @@ from .images import add_transparent_frame
 class CLI(object):
     """Transparent PNG conversion"""
 
-    def convert(self, src, dest):
+    def convert(self, src, dest=None, prefix="t"):
         """
         Output image with transparent PNG
 
@@ -19,7 +19,10 @@ class CLI(object):
 
         """
         path = Path(src)
-        absolute_path = path.resolve()
+        absolute_path = path.expanduser().resolve()
+
+        if dest is None:
+            dest = absolute_path.parent / "-".join([prefix, path.name])
 
         logger.info(f"Open the image: {absolute_path}")
         try:
