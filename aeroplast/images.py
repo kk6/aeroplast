d6d97d216e0157df893fe5cab97608856b27114f
kk6 <hiro.ashiya@gmail.com>
2018-11-15 20:41:33 +0900

    Fixes #4

--- aeroplast/images.py
+++ aeroplast/images.py
@@ -12,7 +12,7 @@ def get_new_size(original_size):
     """
     Returns each width and height plus 2px.
 
-    :param original_size:
+    :param original_size: Original image's size
     :return: Width / height after calculation
     :rtype: tuple
 
@@ -21,9 +21,20 @@ def get_new_size(original_size):
 
 
 def add_transparent_frame(original_image):
+    """
+    Add a transparent frame.
+
+    Generate a transparent image that is 2px larger than the original image,
+    and paste the original image on it.
+
+    :param original_image: Original image
+    :return: Image with transparent frame added.
+    :rtype: Image
+
+    """
     new_size = get_new_size(original_image.size)
     color = (255, 255, 255, 0)
-    image = Image.new(original_image.mode, new_size, color)
+    image = Image.new("RGBA", new_size, color)
     image.paste(original_image, (1, 1))
     return image
 
