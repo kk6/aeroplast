cbde16ca0a0562c5763c79af3a3834d2794ea4c9
kk6 <hiro.ashiya@gmail.com>
2018-11-16 19:57:18 +0900

    Add resize option to convert command.

--- aeroplast/cli.py
+++ aeroplast/cli.py
@@ -3,13 +3,13 @@ from pathlib import Path
 from PIL import Image
 from logzero import logger
 
-from .images import add_transparent_frame
+from .images import add_transparent_frame, resize_image
 
 
 class CLI(object):
     """Transparent PNG conversion"""
 
-    def convert(self, src, dest=None, prefix="t", overwrite=False):
+    def convert(self, src, dest=None, prefix="t", overwrite=False, resize=False):
         """
         Output image with transparent PNG
 
@@ -17,6 +17,8 @@ class CLI(object):
         :param dest: Output destination filename.
         :param prefix: Prefix added to output image file name.
         :param overwrite: Whether to overwrite the image or not.
+        :param resize: Resize the image. When only option is specified, resize to the default 1000 * 1000px.
+                       If you pass a number (pixel) as an optional argument, resize it to fit that size.
         :return: None
 
         """
@@ -37,6 +39,16 @@ class CLI(object):
         except FileNotFoundError:
             logger.error(f"Image not found: {absolute_path}")
         else:
+            if resize:
+                if resize is True:
+                    size = (1000, 1000)
+                else:
+                    size = (resize, resize)
+
+                logger.info(f"Resize the image to fit within {size[0]}*{size[1]} px.")
+                original_image = resize_image(original_image, size)
+                logger.info("Resize complete.")
+
             logger.info("Image converting...")
             image = add_transparent_frame(original_image)
             image.save(dest)
--- tests/test_images.py
+++ tests/test_images.py
@@ -12,3 +12,9 @@ def test_add_transparent_frame(src_image, expected_image):
     from aeroplast.images import add_transparent_frame
     result = add_transparent_frame(src_image)
     assert expected_image.tobytes() == result.tobytes()
+
+
+def test_resize_image(src_image, resized_image):
+    from aeroplast.images import resize_image
+    result = resize_image(src_image, (100, 100))
+    assert resized_image.tobytes() == result.tobytes()
